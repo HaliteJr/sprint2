fc1b3e530e350a97513a636538353998adf7bc66
Noshin Anjum <noshin@dhcp-nyc-658.redrover.cornell.edu>
2017-10-12 22:04:02 -0400

    working on animation

--- myscripts.js
+++ myscripts.js
@@ -7,3 +7,36 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+// function myMove() {
+//     var elem = document.getElementById("mother_ship"); 
+//     var topPos = 300;
+//     var leftPos = 80;
+//     var id = setInterval(frame, 7);
+//     function frame() {
+//         if (leftPos == 350) {
+//             clearInterval(id);
+//         } else {
+//             leftPos++; 
+//             topPos++;
+//             elem.style.top = topPos+ 'px'; 
+//             elem.style.left = leftPos + 'px'; 
+//         }
+//     }
+// }
+
+
+function myLeftMove() {
+    var elem = document.getElementById("mother_ship"); 
+    var leftPos = 80;
+    var id = setInterval(frame, 7);
+    function frame() {
+        if (leftPos == 400) {
+            clearInterval(id);
+        } else {
+            leftPos++; 
+            elem.style.left = leftPos + 'px'; 
+        }
+    }
+}
