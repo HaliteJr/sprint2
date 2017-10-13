8fc2b053a0ca0bdfc9545132de565c09c1a64ae8
Noshin Anjum <noshin@dhcp-nyc-658.redrover.cornell.edu>
2017-10-12 23:02:16 -0400

    added animation

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,4 @@
+
 /* Open the sidenav */
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
@@ -9,30 +10,67 @@ function closeNav() {
 }
 
 
-// function myMove() {
-//     var elem = document.getElementById("mother_ship"); 
-//     var topPos = 300;
-//     var leftPos = 80;
-//     var id = setInterval(frame, 7);
-//     function frame() {
-//         if (leftPos == 350) {
-//             clearInterval(id);
-//         } else {
-//             leftPos++; 
-//             topPos++;
-//             elem.style.top = topPos+ 'px'; 
-//             elem.style.left = leftPos + 'px'; 
-//         }
-//     }
-// }
+
+function goingRedFirst(){
+	myleftMove(700);
+}
+
+function goingBlueFirst(){
+	myRightAngleMove();
+}
 
 
-function myLeftMove() {
-    var elem = document.getElementById("mother_ship"); 
-    var leftPos = 80;
+setTimeout(goingRedFirst(), 10);
+setTimeout(goingRedtoBlue(), 10);
+
+
+
+
+
+function goingRedtoBlue(){
+	var elem = document.getElementById("mother_ship"); 
+    var topPos = 300;
+    var leftPos = 700;
     var id = setInterval(frame, 7);
     function frame() {
-        if (leftPos == 400) {
+        if (leftPos == 900) {
+            clearInterval(id);
+        } else {
+            leftPos++; 
+            topPos = topPos + 1.4;
+            elem.style.top = topPos+ 'px'; 
+            elem.style.left = leftPos + 'px'; 
+        }
+    }
+}
+
+
+
+
+
+function myRightAngleMove() {
+    var elem = document.getElementById("mother_ship"); 
+    var topPos = 300;
+    var leftPos = 80;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (leftPos == 350) {
+            clearInterval(id);
+        } else {
+            leftPos++; 
+            topPos++;
+            elem.style.top = topPos+ 'px'; 
+            elem.style.left = leftPos + 'px'; 
+        }
+    }
+    myleftMove(900);
+}
+function myleftMove(leftPosValue) {
+    var elem = document.getElementById("mother_ship"); 
+    var leftPos = 80;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (leftPos == leftPosValue) {
             clearInterval(id);
         } else {
             leftPos++; 
@@ -40,3 +78,4 @@ function myLeftMove() {
         }
     }
 }
+
